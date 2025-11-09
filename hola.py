afa181d6e868f2ee317342a18c1824df3d72751f
yamilet <maria.gonzalez384@unisalamanca.edu.co>
2025-11-09 16:18:31 -0500

    Primer commit: script de saludo en Python

---
+++ hola.py
@@ -0,0 +1,6 @@
+# hola.py
+# Autor: maria yamilet gonzalez
+# Descripción: Script básico que saluda al usuario
+
+nombre = input("¿Cómo te llamas? ")
+print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
